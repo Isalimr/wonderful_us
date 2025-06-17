30d15bb5dc000b25cc9b315105d798da7e9d78c3
Andre_Carrijo <161145988+Dede-07@users.noreply.github.com>
2025-06-17 20:08:26 -0300

    Update login.js

--- login.js
+++ login.js
@@ -5,7 +5,7 @@ function verificarLogin(event) {
   const senha = document.getElementById("senha").value.trim();
 
   if (usuario === "Dear" && senha === "11/05") {
-    window.location.href = "http://127.0.0.1:5500/wonderful_us.html";
+    window.location.href = "wonderful_us.html";
   } else {
     document.getElementById("mensagem").style.display = "block";
   }
